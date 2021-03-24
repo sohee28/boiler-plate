cdbf10cbb52cdd3484b34bdd853794fd16218957
sohee28 <amybae28@gmail.com>
2021-03-24 08:51:52 -0800

    second heroku

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({ hello: "I am deploying application" });
+});
+
 app.get("/api/user/auth", auth, (req, res) => {
   //only authenticated user can access - middelware
   res.status(200).json({

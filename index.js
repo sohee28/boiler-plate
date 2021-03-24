7caf5df15e821c986ee65426fb05cf941550af31
sohee28 <amybae28@gmail.com>
2021-03-23 20:25:32 -0800

    edit private information

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://SoHee:thgmlqo19960128@boiler-plate.po3cp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => console.log("DB connected"))
   .catch((err) => {
     console.log("there is error");
   });
 
-app.get("/", (req, res) => {
-  res.send("hello world");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.status(200).json({ success: true });
+  });
 });
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+
 const userSchema = mongoose.Schema({
   name: { type: String, maxlength: 50 },
   email: { type: String, trim: true, unique: 1 },

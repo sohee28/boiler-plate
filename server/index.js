234a45c414f35a341469f4e0d2f93a794d109e60
sohee28 <amybae28@gmail.com>
2021-03-24 09:11:39 -0800

    edited heroku

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
-  res.json({ hello: "I am deploying application" });
+  return res.json({ hello: "I am deploying application" });
 });
 
 app.get("/api/user/auth", auth, (req, res) => {
